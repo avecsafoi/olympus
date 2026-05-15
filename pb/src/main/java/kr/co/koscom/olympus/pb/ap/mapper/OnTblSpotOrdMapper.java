e9a6aafe16b2afa12cedb944c294dba0087437bc
avecsa <avecsafoi@gmail.com>
2026-05-15 11:09:30 +0900

    .

--- pb/src/main/java/kr/co/koscom/olympus/pb/ap/mapper/OnTblSpotOrdMapper.java
+++ pb/src/main/java/kr/co/koscom/olympus/pb/ap/mapper/OnTblSpotOrdMapper.java
@@ -30,20 +30,6 @@ public interface OnTblSpotOrdMapper extends PBMapper<OnTblSpotOrd> {
     @Select("select * from on_tbl_spot_ord where ord_dt >= #{qi.ordDt} LIMIT #{qi.offset}, #{qi.limit}")
     List<OnTblSpotOrd> page02(@Param("qi") OrdQi qi);
 
-    @Select("select * from on_tbl_spot_ord where ord_dt >= #{q1.ordDt} LIMIT #{q2.offset}, #{q2.limit}")
-    List<OnTblSpotOrd> page03(OrdQi q1, OrdQi q2);
-
-    @Select("select * from on_tbl_spot_ord where ord_dt >= #{qx1.ordDt} LIMIT #{qx2.offset}, #{qx2.limit}")
-    List<OnTblSpotOrd> page04(OrdQi qx1, @Param("qx2") OrdQi q2);
-
-    @Select("select * from on_tbl_spot_ord where ord_dt >= #{qi.ordDt}")
-    List<OnTblSpotOrd> page40(OrdQi qi, PBNPage pg);
-
-    List<OnTblSpotOrd> page50(@Param("qi") OrdQi qi, @Param("pg") PBCPage pg);
-
-    @Select("select * from on_tbl_spot_ord where ord_dt >= #{qi.ordDt}")
-    List<OnTblSpotOrd> page51(PBCPage pg, OrdQi qi);
-
     @Select("select * from on_tbl_spot_ord where ord_dt >= #{ordDt}")
     List<OnTblSpotOrd> page52(OrdQiWithCPage qi);
 
